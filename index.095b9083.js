d2267671f0eb5ce822eaa131eca50a81e1af4aa9
OlhaHobchuk <111050433+OlhaHobchuk@users.noreply.github.com>
2023-08-29 11:45:00 +0000

    Deploying to gh-pages from @ OlhaHobchuk/IceCream-Shop@ef1c60e0b34bbb6c77397786d760175422d795ca 🚀

---
+++ index.095b9083.js
@@ -0,0 +1,2 @@
+new Swiper(".swiper",{direction:"horizontal",loop:!0,navigation:{nextEl:".next",prevEl:".prev"},effect:"coverflow",slidesPerView:4,centeredSlide:!0,autoplay:{delay:1e3}});
+//# sourceMappingURL=index.095b9083.js.map
---
+++ index.3cdf3de2.js
@@ -0,0 +1,2 @@
+new Swiper(".swiper",{direction:"horizontal",loop:!0,navigation:{nextEl:".next",prevEl:".prev"},effect:"coverflow",slidesPerView:4,centeredSlide:!0,autoplay:{delay:1e3}});
+//# sourceMappingURL=index.3cdf3de2.js.map
